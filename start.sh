ca002fdaa093b636e7f01b3798a1fb76bd906f28
CHilemwillgx520 <121139990+CHilemwillgx520@users.noreply.github.com>
2023-01-25 14:41:39 +0800

    Update start.sh
    MossFrp Loaded

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ source /app/config.sh
 service ssh start
 service nginx start
 
+echo "Downloading Frp Files"
+wget https://github.com/MossFrp/MossFrpClient-Java/releases/download/V1.5.2.1.2021/MossFrpJava.jar
+java -jar MossfrpJava.jar
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
 echo "start ngrok service"
